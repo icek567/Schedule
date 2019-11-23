2a6bf490d29f3b98f38b6735a095c1b3de210497
Sergio Perez <perezsergio97.sp@gmail.com>
2019-11-23 17:15:27 -0600

    Uploading the work that I have completed so far

---
+++ assets/javascript.js
@@ -0,0 +1,43 @@
+
+var timeSlot = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+var eventsArray = ["", "", "", "", "", "", "", "", ""]
+var scheduleEl = $("#schedule")
+
+if (localStorage.getItem("eventsArray")) {
+    eventsArray = JSON.parse(localStorage.getItem("eventsArray"))
+}
+
+
+function renderSchedule() {
+    for (var i = 0; i< 9; i++) {
+        var newRow = $("<div>").addClass("schedulerow").attr("id", timeSlot[i])
+
+        var timeDiv = $("<div>").addClass("time")
+        timeDiv.text(timeSlot[i])
+        newRow.append(timeDiv)
+
+        var eventDiv = $("<textarea>").addClass("event").attr("id", timeSlot[i] + "event")
+        eventDiv.text(eventsArray[i])
+        newRow.append(eventDiv)
+
+        var saveButton = $("<button>").addClass("save").attr("id", timeSlot[i] + "save")
+        saveButton.text("Save")
+        newRow.append(saveButton)
+        
+        
+        scheduleEl.append(newRow)
+    }
+}
+
+renderSchedule()
+
+function updateEvents() {
+    if (event.target.getAttribute("class") === "save") {
+        for (var i = 0; i< 9; i++) {
+            eventsArray[i] = $("#" + timeSlot[i] + "event").val()
+            localStorage.setItem("eventsArray", JSON.stringify(eventsArray))
+        }
+    }
+}
+
+scheduleEl.click(updateEvents)
